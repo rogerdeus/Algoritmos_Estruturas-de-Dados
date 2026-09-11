222a5310bd073b80687d221fbc133fb291d81124
zero2 <joyparker881@gmail.com>
2026-09-11 09:04:57 -0300

    add: implement removal and attendance functions in deque structure

--- Deque.c
+++ Deque.c
@@ -3,6 +3,8 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+
+int contador =0;
 //modelagem da estrutura de dados
 
 struct no
@@ -32,7 +34,7 @@ struct no *inserir(struct no *fila, int numero){
 ////////////////////////////////
 //
 
-struct no *remove(struct no *fila){
+struct no *remover(struct no *fila){
     //caso muito facil fila vazia
     if(fila == NULL){
         printf("va tomar um cafe");
@@ -46,7 +48,56 @@ struct no *remove(struct no *fila){
     }
     //caso dificil tem mais de um no na fila
 
+    struct no *penultimo =fila;
+    while (penultimo ->proximo ->proximo != NULL)
+    {
+        penultimo = penultimo ->proximo;
+    }
+    printf("%d\n", penultimo->proximo->proximo);
+    free(penultimo->proximo);
+    penultimo->proximo=NULL;
+    return fila;
+
+    //função que realiza o atendimento
+
+
+
     
+}
+
+void atender(struct Deque *cabeca){
+    //caso muito facil duas filas vazias
+//aqui não tem ninguem
+    if((cabeca->filaNormal ==NULL) && (cabeca->filapreferencial == NULL)){
+        contador = 1;
+        return;
+    }
+//aqui so tem normal
+    if(cabeca->filapreferencial ==NULL){
+        cabeca->filaNormal = remover(cabeca->filaNormal);
+    }
+
+    //caso facil so tem preferencial
+
+    if(cabeca->filaNormal ==NULL){
+        cabeca ->filapreferencial = remover(cabeca->filapreferencial);
+        contador = 1;
+        return;
+    }
+
+    //caso dificil tem gente em ambas as filas
+
+    if((contador % 4) == 0){
+        cabeca->filaNormal = remover(cabeca->filaNormal);
+    }else
+    {
+        cabeca->filapreferencial = remover(cabeca->filapreferencial);
+    }
+    contador++;
+
+
+    
+
 }
 
 
@@ -56,5 +107,11 @@ struct no *remove(struct no *fila){
 
 
 
+//funcao de execução de programa
+int main(){
+    struct Deque *cabeca = (struct Deque*) malloc(sizeof(struct Deque));
+    cabeca->filaNormal = NULL;
+    cabeca->filapreferencial =NULL;
 
-int main(){}
+    
+}

6f1c7589c0d77f5ddbed8eaa8264f2e74cde8d36
zero2 <joyparker881@gmail.com>
2026-09-18 09:23:40 -0300

    fix: correct pointer assignment in inserir function for sparse matrix

--- matriz_esparsa.c
+++ matriz_esparsa.c
@@ -60,7 +60,44 @@ void inserir(int numero){
 
     struct no *novoNO = (struct no *)malloc(sizeof(struct no));
     novoNO->numero=numero;
-    novoNO->proximoNO=
+    novoNO->proximoNO=ponteirodiretor->proximoNO;
+    ponteirodiretor->proximoNO=novoNO;
+}
+
+void excluir(int numero){
+    struct diretor *ponteirodiretor = procuradiretor(numero);
+    //senario muit facil não tem numero
+
+    if (ponteirodiretor->proximoNO == NULL)
+    {
+        return;
+    }
+
+    if(ponteirodiretor-> proximoNO->numero == numero){
+        struct no *limpeza =ponteirodiretor->proximoNO;
+        ponteirodiretor->proximoNO=ponteirodiretor->proximoNO->proximoNO;
+        free(limpeza);
+
+    }
+
+    //senario dificil procurar pelo numero a ser excluidos parar no anterior
+    struct no *anterior = ponteirodiretor->proximoNO;
+    while ((anterior->proximoNO!=NULL) && (anterior->proximoNO->numero != numero))
+    {
+        anterior= anterior->proximoNO;
+    }
+
+    //senario não encontrei
+    if(anterior->proximoNO ==NULL){
+        return;
+    }
+
+    struct no *limpeza =anterior->proximoNO;
+    anterior->proximoNO=anterior->proximoNO->proximoNO;
+    free(limpeza);
+    
+
+
 }
 
 
